7e30ddb36838d2ee181eddb24406a49709bab134
Chris Slowik <chris.slowik@gmail.com>
2026-04-29 22:18:35 -0500

    Add Level / PlanetData / TargetZoneData Codable structs

--- Free Return/Levels/Level.swift
+++ Free Return/Levels/Level.swift
@@ -9,3 +9,22 @@ struct Vec2: Codable, Equatable {
     let x: CGFloat
     let y: CGFloat
 }
+
+struct PlanetData: Codable, Equatable {
+    let position: Vec2
+    let mass: CGFloat
+    let radius: CGFloat
+}
+
+struct TargetZoneData: Codable, Equatable {
+    let position: Vec2
+    let radius: CGFloat
+}
+
+struct Level: Codable, Equatable {
+    let id: String
+    let name: String
+    let shipStart: Vec2
+    let planets: [PlanetData]
+    let target: TargetZoneData
+}
